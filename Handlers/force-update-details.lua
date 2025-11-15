dd53bcb6a559e667c4e3f348e281782484671059
Mattes D <github@xoft.cz>
2025-11-15 21:58:53 +0100

    Added forceUpdateDetails.

---
+++ Handlers/force-update-details.lua
@@ -0,0 +1,26 @@
+-- force-update-details.lua
+
+--[[ Handles the request to force an update to details even when they are already in the DB
+--]]
+
+
+
+
+
+local httpResponse = require("httpResponse")
+local requestQueue = require("requestQueue")
+
+
+
+
+
+return function(aClient, aPath, aParams, aHeaders)
+	local aId = tonumber(aPath:match("^/force%-update%-details/(%d+)$"))
+	if not(aId) then
+		return httpResponse.send(aClient, 400, "text/plain", "Invalid aId")
+	end
+
+	requestQueue.addToFront(aId)
+
+	httpResponse.sendRedirect(aClient, "/anime/" .. tostring(aId))
+end
